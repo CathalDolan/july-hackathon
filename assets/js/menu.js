a1079b3831804cf8398c441f51e93debb230480e
Bryan Mullen <bryan@bryanmullen.me>
2020-07-08 12:21:55 +0100

    Partial js logic

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,27 +1,59 @@
 // Drinks Variables
-const espressoSm = document.querySelector("#espresso-sm");
-const espressoLg = document.querySelector("#espresso-lg");
-const americanoSm = document.querySelector("#americano-sm");
-const americanoLg = document.querySelector("#americano-lg");
-const macchiatoSm = document.querySelector("#macchiato-sm");
-const macchiatoLg = document.querySelector("#macchiato-lg");
-const mochaSm = document.querySelector("#mocha-sm");
-const mochaLg = document.querySelector("#mocha-lg");
-const cappucinoSm = document.querySelector("#cappucino-sm");
-const cappucinoLg = document.querySelector("#cappucino-lg");
-const flatWhiteSm = document.querySelector("#flat-white-sm");
-const flatWhiteLg = document.querySelector("#flat-white-lg");
-const latteSm = document.querySelector("#latte-sm");
-const latteLg = document.querySelector("#latte-lg");
-const tigerEyeSm = document.querySelector("#tiger-eye-sm");
-const tigerEyeLg = document.querySelector("#tiger-eye-lg");
-const teaSm = document.querySelector("#tea-sm");
-const teaLg = document.querySelector("#tea-lg");
+const espressoSmInput = document.querySelector("#espresso-sm");
+const espressoLgInput = document.querySelector("#espresso-lg");
+const americanoSmInput = document.querySelector("#americano-sm");
+const americanoLgInput = document.querySelector("#americano-lg");
+const macchiatoSmInput = document.querySelector("#macchiato-sm");
+const macchiatoLgInput = document.querySelector("#macchiato-lg");
+const mochaSmInput = document.querySelector("#mocha-sm");
+const mochaLgInput = document.querySelector("#mocha-lg");
+const cappucinoSmInput = document.querySelector("#cappucino-sm");
+const cappucinoLgInput = document.querySelector("#cappucino-lg");
+const flatWhiteSmInput = document.querySelector("#flat-white-sm");
+const flatWhiteLgInput = document.querySelector("#flat-white-lg");
+const latteSmInput = document.querySelector("#latte-sm");
+const latteLgInput = document.querySelector("#latte-lg");
+const tigerEyeSmInput = document.querySelector("#tiger-eye-sm");
+const tigerEyeLgInput = document.querySelector("#tiger-eye-lg");
+const teaSmInput = document.querySelector("#tea-sm");
+const teaLgInput = document.querySelector("#tea-lg");
 
 // Food Variables
-const teaLg = document.querySelector("#fruit-scone");
-const teaLg = document.querySelector("#carrot-cake");
-const teaLg = document.querySelector("#danish");
-const teaLg = document.querySelector("#sausage-roll");
-const teaLg = document.querySelector("#protein-bar");
-const teaLg = document.querySelector("#cheese-toasty");
+const fruitSconeInput = document.querySelector("#fruit-scone");
+const carrotCakeInput = document.querySelector("#carrot-cake");
+const danishInput = document.querySelector("#danish");
+const sausageRollInput = document.querySelector("#sausage-roll");
+const proteinBarInput = document.querySelector("#protein-bar");
+const cheeseToastyInput = document.querySelector("#cheese-toasty");
+
+let order;
+
+
+// document.addEventListener(onkeypress, (() => {
+//   order = {
+//     espressoSm: espressoSmInput,
+//     espressoLg: espressoLgInput,
+//     americanoSm: americanoSmInput,
+//     americanoLg: americanoLgInput,
+//     macchiatoSm: macchiatoSmInput,
+//     macchiatoLg: macchiatoLgInput,
+//     mochaSm: mochaSmInput,
+//     mochaLg: mochaLgInput,
+//     cappucinoSm: cappucinoSmInput,
+//     cappucinoLg: cappucinoLgInput,
+//     flatWhiteSm: flatWhiteSmInput,
+//     flatWhiteLg: flatWhiteLgInput,
+//     latteSm: latteSmInput,
+//     latteLg: latteLgInput,
+//     tigerEyeSm: tigerEyeSmInput,
+//     tigerEyeLg: tigerEyeLgInput,
+//     teaSm: teaSmInput,
+//     teaLg: teaLgInput,
+//     fruitScone: fruitSconeInput,
+//     carrotCake: carrotCakeInput,
+//     danish: danishInput,
+//     sausageRoll: sausageRollInput,
+//     proteinBar: proteinBarInput,
+//     cheeseToasty: cheeseToastyInput,
+//   }
+// }))
