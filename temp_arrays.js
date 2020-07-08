137610bb0874c869295e020e0a2c45b3fcdce302
CathalDolan <cathal@gizagig.com>
2020-07-08 10:02:46 +0000

    id's added to products. Pricing array created

---
+++ temp_arrays.js
@@ -0,0 +1,75 @@
+const menu = [
+    {name:"Espresso", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""
+    },
+    {name:"Americano", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""
+    },
+    {name:"Macchiato", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""
+    },
+    {name:"Mocha", 
+        price6oz:3.50, 
+        price8oz:3.95, 
+        price:""
+    },
+    {name:"Cappucino", 
+        price6oz:3.25, 
+        price8oz:3.75, 
+        price:""
+    },
+    {name:"Flat White", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""
+    },
+    {name:"Latte", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""},
+    {name:"Tiger Eye", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""},
+    {name:"Tea", 
+        price6oz:2.95, 
+        price8oz:2.50, 
+        price:""
+    },
+    {name:"Fran's Mum's Fruit Scone", 
+        price6oz:"", 
+        price8oz:"", 
+        price:1.95
+    },
+    {name:"Carrot Cake", 
+        price6oz:"", 
+        price8oz:"", 
+        price:3.50
+    },
+    {name:"Our Danish", 
+        price6oz:"", 
+        price8oz:"", 
+        price:2.95
+    },
+    {name:"Sausage Rolls", 
+        price6oz:"", 
+        price8oz:"", 
+        price:2.50
+    },
+    {name:"Homemade Protein Bar", 
+        price6oz:"", 
+        price8oz:"", 
+        price:3.95
+    },
+    {name:"Ham & Cheese Toasty", 
+        price6oz:"", 
+        price8oz:"", 
+        price:4.95
+    },
+]
