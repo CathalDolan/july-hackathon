a25e48a0c4f024463e028c367851f1bb9d6939fb
Bryan Mullen <bryan@bryanmullen.me>
2020-07-08 12:08:21 +0100

    Add IDs to all food and drinks items in the menu, and create menu.js assigning variables

---
+++ assets/js/menu.js
@@ -0,0 +1,27 @@
+// Drinks Variables
+const espressoSm = document.querySelector("#espresso-sm");
+const espressoLg = document.querySelector("#espresso-lg");
+const americanoSm = document.querySelector("#americano-sm");
+const americanoLg = document.querySelector("#americano-lg");
+const macchiatoSm = document.querySelector("#macchiato-sm");
+const macchiatoLg = document.querySelector("#macchiato-lg");
+const mochaSm = document.querySelector("#mocha-sm");
+const mochaLg = document.querySelector("#mocha-lg");
+const cappucinoSm = document.querySelector("#cappucino-sm");
+const cappucinoLg = document.querySelector("#cappucino-lg");
+const flatWhiteSm = document.querySelector("#flat-white-sm");
+const flatWhiteLg = document.querySelector("#flat-white-lg");
+const latteSm = document.querySelector("#latte-sm");
+const latteLg = document.querySelector("#latte-lg");
+const tigerEyeSm = document.querySelector("#tiger-eye-sm");
+const tigerEyeLg = document.querySelector("#tiger-eye-lg");
+const teaSm = document.querySelector("#tea-sm");
+const teaLg = document.querySelector("#tea-lg");
+
+// Food Variables
+const teaLg = document.querySelector("#fruit-scone");
+const teaLg = document.querySelector("#carrot-cake");
+const teaLg = document.querySelector("#danish");
+const teaLg = document.querySelector("#sausage-roll");
+const teaLg = document.querySelector("#protein-bar");
+const teaLg = document.querySelector("#cheese-toasty");
